227abc65a1092cfeb09049c2f11f6bd2e58b2764
Brave2004 <brave2004@gmail.com>
2025-09-18 11:00:39 +0500

    mvc

--- src/main/java/G50/config/AppConfig.java
+++
@@ -1,9 +0,0 @@
-package G50.config;
-
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@ComponentScan("G50.Model")
-public class AppConfig {
-}
